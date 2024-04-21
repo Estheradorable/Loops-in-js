47102115f56dd56fc6223ca535299bea4678dc19
Estheradorable <113927032+Estheradorable@users.noreply.github.com>
2024-04-21 16:22:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+//imagine we want to display 'Hello world' five times on on the console
+/*console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+
+
+
+
+
+
+
+
+
+
+//for loop
+for(let i=5; i>=1; i--){
+  console.log("Hello world")
+}
+
+
+for(let i=1; i<=10; i++){
+  console.log(i)}*/
+
+
+
+
+for (let i = 1; i<=10; i++){
+  if(i % 2 ==0){
+    console.log(i)
+  }
+}
+
+
+
+
+/*for(let i=5; i >= 1; i--){
+  console.log(i)
+}*/
+
+/*function alpha(){
+  const alphaArray =[];
+  for (let i =90; i>=65; i--){
+    alphaArray.push(String.fromCharCode(i))
+  }
+  return alphaArray
+}
+console.log(alpha())*/
+
